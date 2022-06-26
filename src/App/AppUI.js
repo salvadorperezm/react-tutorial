0ddc86a7f02fa6b712428281e56ad61cdb92b7eb
salvadorperezm <salvadorperezm115@gmail.com>
2022-06-26 16:12:37 -0600

    apunto de empezar la clase 18

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,6 +7,8 @@ import { CreateTodoButton } from "../CreateTodoButton";
 import { TodoContext } from "../TodoContext";
 
 function AppUI() {
+  const { error, loading, searchedTodos, completeTodo, deleteTodo } =
+    React.useContext(TodoContext);
   return (
     <div className="hero-container">
       <React.Fragment>
@@ -15,28 +17,20 @@ function AppUI() {
           <TodoSearch />
           <CreateTodoButton />
         </div>
-        <TodoContext.Consumer>
-          {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => {
-            return (
-              <TodoList>
-                {error && <p>Hubo un error...</p>}
-                {loading && <p>La pagina esta cargando</p>}
-                {!loading && !searchedTodos.length && (
-                  <p>Crea tu primera tarea</p>
-                )}
-                {searchedTodos.map((todo) => (
-                  <TodoItem
-                    key={todo.text}
-                    text={todo.text}
-                    completed={todo.completed}
-                    onComplete={() => completeTodo(todo.text)}
-                    onDelete={() => deleteTodo(todo.text)}
-                  />
-                ))}
-              </TodoList>
-            );
-          }}
-        </TodoContext.Consumer>
+        <TodoList>
+          {error && <p>Hubo un error...</p>}
+          {loading && <p>La pagina esta cargando</p>}
+          {!loading && !searchedTodos.length && <p>Crea tu primera tarea</p>}
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
       </React.Fragment>
     </div>
   );
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./TodoCounter.css";
+import { TodoContext } from "../TodoContext";
 
-function TodoCounter({ total, completed }) {
+function TodoCounter() {
+  const { totalTodos, completedTodos } = React.useContext(TodoContext);
   return (
     <h2 className="TodoCounter">
-      Has completado {completed} de {total} Tareas
+      Has completado {completedTodos} de {totalTodos} Tareas
     </h2>
   );
 }
